377506f432b56e9ab6741463c3b8cefbee0d158b
lokman <lokmanhossen230@gmail.com>
2022-11-06 09:37:59 +0600

    Add Button Icon

--- heading_title/lib/main.dart
+++ heading_title/lib/main.dart
@@ -10,21 +10,71 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //buildButton function
+    Column buildButton(IconData icon, String label) {
+      Color color = new Color(0xff622F74);
+      return Column(
+        children: [
+          Icon(icon, color: color),
+          Container(
+            child: Text(
+              label,
+              style: TextStyle(
+                color: color,
+                fontSize: 20,
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
+    //buttonDisplay
+    Widget buttonDisplay = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          buildButton(Icons.phone, Strings.appButtoncall),
+          buildButton(Icons.airplanemode_active, Strings.appButtonAirplane),
+          buildButton(Icons.chat, Strings.appButtonChat),
+          buildButton(Icons.radio, Strings.appButtonMusic),
+        ],
+      ),
+    );
+
+    // itleSection widget
     Widget titleSection = Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          Text(
-            Strings.appHeadingTitle,
-            style: TextStyle(
-              color: Color(0xff622F74),
-              fontWeight: FontWeight.bold,
-              fontSize: 50.0,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              Strings.appHeadingTitle,
+              style: TextStyle(
+                color: Color(0xff622F74),
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                Strings.appsubHeadingTitle,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 31, 165, 91),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Heading Title',
@@ -37,7 +87,7 @@ class MyApp extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            //Text(Strings.appHeadingTitle),
+            buttonDisplay,
             titleSection,
           ],
         ),
--- heading_title/lib/strings.dart
+++ heading_title/lib/strings.dart
@@ -2,4 +2,12 @@ class Strings {
   static final appBarTitle = "Title Screen";
   static final appHeadingTitle = "This is my Heading";
   static final appsubHeadingTitle = "This is my Sub Heading";
+
+  static String appButtoncall = "Buttons";
+  static String appButtonAirplane = "Airplane";
+  static String appButtonChat = "Chat";
+  static String appButtonMusic = "Music";
+ 
+
+  
 }
