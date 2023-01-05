4c04e290750b4f223c7a0d56bedb207b238bf309
lokman <lokmanhossen230@gmail.com>
2023-01-05 23:27:05 +0600

    Add Image and Assets

---
+++ image_and_assats/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(" Image And Assets"),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: const Center(
+          child:
+              // Image.asset(
+              //   'image/img-3.jpg',
+              // ),
+
+              Image(
+            image: AssetImage('image/img-1.jpg'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("click"),
+          backgroundColor: Colors.red,
+        ),
+      ),
+    );
+  }
+}
