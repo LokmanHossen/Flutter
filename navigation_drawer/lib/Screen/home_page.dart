2e99c7130511bd61f913c2cb5136281f013309d4
lokman <lokmanhossen230@gmail.com>
2023-01-13 02:20:55 +0600

    Add Bottom NavigationBar

--- navigation_drawer/lib/Screen/home_page.dart
+++ navigation_drawer/lib/Screen/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
@@ -14,6 +15,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.teal[100],
       appBar: AppBar(
         title: const Text('Navigation Drawer'),
       ),
@@ -98,15 +100,105 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         child: ListView.builder(
-            itemCount: product.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(child: Text(product[index][0]),),
-                title: Text(product[index]),
-                subtitle: Text(productDetails[index]),
-                trailing: Text(price[index].toString()),
-              );
-            }),
+          itemCount: product.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                child: Text(product[index][0]),
+              ),
+              title: Text(product[index]),
+              subtitle: Text(productDetails[index]),
+              trailing: Text(price[index].toString()),
+            );
+          },
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+        foregroundColor: Colors.yellow,
+        // shape: BeveledRectangleBorder(
+        //   borderRadius: BorderRadius.circular(20.0),
+        //   side: BorderSide(color: Colors.blue, width: 2.0,style: BorderStyle.solid),
+        // ),
+        // mini: true,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 5.0,
+        shape: CircularNotchedRectangle(),
+        color: Colors.grey[800],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(
+                    Icons.home,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "Home",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 50.0, top: 10.0, bottom: 10.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(
+                    Icons.shop,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "Shop",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 50.0, top: 10.0, bottom: 10.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "Fav",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 10.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "Settings",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
