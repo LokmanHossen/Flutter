1d297864c600c4efa080e48e9b408fc585b02e85
LokmanHossen <lokmanhossen230@gmail.com>
2023-05-24 08:12:54 +0600

    VerifyEmailView

--- my_note/lib/main.dart
+++ my_note/lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:js';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_note/Views/login_view.dart';
 
 import 'firebase_options.dart';
 
@@ -32,12 +37,39 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                // print('You are verified user');
+              } else {
+                //print('You need to verify your Email first');
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const VerifyEnmailView(),
+                  ),
+                );
+              }
               return Text("Done");
+            // print(FirebaseAuth.instance.currentUser);
+
             default:
               return const Text('Loading...');
           }
         },
       ),
     );
+  }
+}
+
+class VerifyEnmailView extends StatefulWidget {
+  const VerifyEnmailView({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyEnmailView> createState() => _VerifyEnmailViewState();
+}
+
+class _VerifyEnmailViewState extends State<VerifyEnmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
