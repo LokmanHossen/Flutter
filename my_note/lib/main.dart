03beace1ead344150bbc1c433500f0583f062768
LokmanHossen <lokmanhossen230@gmail.com>
2023-05-27 01:28:09 +0600

    Log Out

--- my_note/lib/main.dart
+++ my_note/lib/main.dart
@@ -90,8 +90,20 @@ class _NotesViewState extends State<NotesView> {
         title: const Text('Main UI'),
         actions: [
           PopupMenuButton<MenuAction>(
-            onSelected: (value) {
-              devtools.log(value.toString());
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/login/', (_) => false);
+                  }
+                // devtools.log(shouldLogout.toString());
+
+                // break;
+              }
+              // devtools.log(value.toString());
             },
             itemBuilder: (context) {
               return const [
@@ -107,4 +119,28 @@ class _NotesViewState extends State<NotesView> {
       body: const Text('Hello wprld'),
     );
   }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sign out'),
+        content: const Text('Are you sure you want to sign out'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Cancel')),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Log out')),
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
 }
