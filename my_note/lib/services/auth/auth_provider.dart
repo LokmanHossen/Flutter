cdecc0680f702b97f24c14c002bc8a05e5e378a1
LokmanHossen <lokmanhossen230@gmail.com>
2023-06-04 18:28:23 +0600

    Auth Provider

--- my_note/lib/services/auth/auth_provider.dart
+++ my_note/lib/services/auth/auth_provider.dart
@@ -1,8 +1,7 @@
-import 'dart:ffi';
-
 import 'package:my_note/services/auth/auth_user.dart';
 
 abstract class AuthProvider {
+  Future<void> initialize();
   AuthUser? get currentUser;
   Future<AuthUser> logIn({
     required String email,
