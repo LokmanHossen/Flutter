c093f5605e00b82937321aa0fdd015762b400829
LokmanHossen <lokmanhossen230@gmail.com>
2023-07-20 13:27:25 +0600

    crud exceptions

---
+++ my_note/lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,15 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDoumentsDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CounlNotFindUser implements Exception {}
+
+class CounlNotFindNote implements Exception {}
+
+class CounlNotUpdateNote implements Exception {}
