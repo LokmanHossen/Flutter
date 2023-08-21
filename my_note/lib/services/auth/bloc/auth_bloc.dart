6c48562979ae66cfa05adb64494b920d8ebe8b6f
LokmanHossen <lokmanhossen230@gmail.com>
2023-08-22 00:14:04 +0600

    Initialize and LogIn bloc

---
+++ my_note/lib/services/auth/bloc/auth_bloc.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_note/services/auth/auth_provider.dart';
+import 'package:my_note/services/auth/bloc/auth_event.dart';
+import 'package:my_note/services/auth/bloc/auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc(AuthProvider provider) : super(const AuthStateLoading()) {
+    //Initialized
+    on<AuthEventInitialize>((event, emit) async {
+      await provider.initialize();
+      final user = provider.currentUser;
+      if (user == null) {
+        emit(const AuthStateLoggedOut());
+      } else if (!user.isEmailVerified) {
+        emit(const AuthStateNeedsVerification());
+      } else {
+        emit(AuthStateLoggedIn(user));
+      }
+    });
+    // Log In
+    on<AutheventLogIn>((event, emit) async {
+      emit(const AuthStateLoading());
+      final email = event.email;
+      final password = event.password;
+      try {
+        final user = await provider.logIn(
+          email: email,
+          password: password,
+        );
+        emit(AuthStateLoggedIn(user));
+      } on Exception catch (e) {
+        emit(AuthStateLoginFailure(e));
+      }
+    });
+  }
+}
