6739fa3ee5ff907144b94adab649ebe4f7d1d217
LokmanHossen <lokmanhossen230@gmail.com>
2023-08-25 13:02:47 +0600

    update

--- my_note/lib/services/auth/bloc/auth_state.dart
+++ my_note/lib/services/auth/bloc/auth_state.dart
@@ -10,23 +10,23 @@ class AuthStateLoading extends AuthState {
   const AuthStateLoading();
 }
 
-
 class AuthStateLoggedIn extends AuthState {
   final AuthUser user;
   const AuthStateLoggedIn(this.user);
 }
 
-class AuthStateLoginFailure extends AuthState {
-  final Exception exception;
-  const AuthStateLoginFailure(this.exception);
-}
+// class AuthStateLoginFailure extends AuthState {
+//   final Exception exception;
+//   const AuthStateLoginFailure(this.exception);
+// }
 
 class AuthStateNeedsVerification extends AuthState {
   const AuthStateNeedsVerification();
 }
 
 class AuthStateLoggedOut extends AuthState {
-  const AuthStateLoggedOut();
+  final Exception? exception;
+  const AuthStateLoggedOut(this.exception);
 }
 
 class AuthStateLogoutFailure extends AuthState {
